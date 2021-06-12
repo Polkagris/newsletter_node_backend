e69c821b2d7ffbc82a3704e110265eca7b037d25
unknown <arne.n.bjerkaas@gmail.com>
2021-06-12 22:58:18 +0200

    Add user from local with simpler script

--- app.js
+++ app.js
@@ -1,93 +1,34 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const path = require("path");
-const fetch = require("node-fetch");
-const axios = require("axios");
+const mailchimp = require("@mailchimp/mailchimp_marketing");
 
-const app = express();
-// const environmentVariables = require("./.env");
-const { request } = require("http");
-// const { response } = require("express");
-// import API_KEY from "./env";
-
-// Bodyparser Middleware
-app.use(bodyParser.urlencoded({ extended: true }));
-
-// Signup Route
-app.post("/signup", (req, res) => {
-  const { firstName, lastName, email } = req.body;
-
-  // Construct req data
-  const data = {
-    members: [
-      {
-        email_address: email,
-        status: "subscribed",
-        merge_fields: {
-          FNAME: firstName,
-          LNAME: lastName,
-        },
-      },
-    ],
-  };
-
-  const postData = JSON.stringify(data);
-
-  try {
-    axios
-      .post(
-        "https://us6.api.mailchimp.com/3.0/lists/3e96be85fb",
-        {
-          postData,
-        },
-        {
-          headers: {
-            Authorization: `auth ${process.env.API_KEY}`,
-          },
-        }
-      )
-      .then((data) => res.status(200).send(data))
-      .catch((err) => res.send(err));
-  } catch (err) {
-    console.error("GG", err);
-  }
+mailchimp.setConfig({
+  apiKey: "07baea80ed7e6a3ffd70e2b8e75b273f-us6",
+  server: "us6",
 });
 
-/*   fetch("https://us6.api.mailchimp.com/3.0/lists/3e96be85fb", {
-    method: "POST",
-    headers: {
-      Authorization: `auth ${environmentVariables.API_KEY}`,
-    },
-    body: postData,
-  })
-    .then(
-      res.sendStatus(200),
-      console.log("Success API key: ", environmentVariables.API_KEY)
-    )
-    .catch((err) => console.log(err));
-}); */
+const listId = "3e96be85fb";
+const subscribingUser = {
+  firstName: "Rick",
+  lastName: "Sanches",
+  email: "ricksanches@local.com",
+};
 
-/*   const options = {
-    url: "https://us6.api.mailchimp.com/3.0/lists/3e96be85fb",
-    method: "POST",
-    headers: {
-      Authorization: "auth 342e071feee30afd363fb8c757c6c1aa-us6",
-    },
-    body: postData,
-  };
+async function run() {
+  try {
+    const response = await mailchimp.lists.addListMember(listId, {
+      email_address: subscribingUser.email,
+      status: "subscribed",
+      merge_fields: {
+        FNAME: subscribingUser.firstName,
+        LNAME: subscribingUser.lastName,
+      },
+    });
 
-  request(options, (err, response, body) => {
-    if (err) {
-      res.send("Error with something:", err);
-    } else {
-      if (response.status === 200) {
-        res.send("It works with status 200");
-      }
-      res.send("Its works: ", body);
-    }
-  });
-}); */
+    console.log(
+      `Successfully added contact as an audience member. The contact's id is ${response.id}.`
+    );
+  } catch (err) {
+    console.log("error:", err);
+  }
+}
 
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, console.log(`Server started on ${PORT}`));
+run();
